a33b63d92fc7d52974c8d399201d777253453dab
PULICCE MATEO <46956808@ort>
2022-10-21 10:49:50 -0300

    u

--- Assets/Scripts/Muere.cs
+++ Assets/Scripts/Muere.cs
@@ -6,37 +6,12 @@ using UnityEngine.UI;
 
 public class Muere : MonoBehaviour
 {
-    public GameObject perdio;
-    public float textTime;
-    float currTextTime;
-
-    void Start()
-    {
-        perdio.SetActive(!perdio.activeInHierarchy);
-        currTextTime = textTime;
-    }
-
-    void Update()
-    {
-        if (perdio.activeInHierarchy)
-        {
-            currTextTime -= Time.deltaTime;
-            if (currTextTime <= 0)
-            {
-                perdio.SetActive(false);
-                currTextTime = textTime;
-            }
-
-        }
-    }
     public void OnCollisionEnter (Collision col)
     {
         if (col.gameObject.name == "Sillas")
         {
-            perdio.SetActive(true);
             Debug.Log("Colisiona");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            perdio.SetActive(true);
         }
     }
 }
